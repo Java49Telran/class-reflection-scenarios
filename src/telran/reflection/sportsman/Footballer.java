df083e3edddca86fb20b1ddeee27e79467bd45e6
Yuragranovsky <yuragranovsky@gmail.com>
2023-10-22 21:08:03 +0300

    CW #53 creating object by class name as a string

---
+++ src/telran/reflection/sportsman/Footballer.java
@@ -0,0 +1,17 @@
+package telran.reflection.sportsman;
+
+public class Footballer implements Sportsman {
+String team;
+
+	public Footballer(String team) {
+	super();
+	this.team = team;
+}
+
+	@Override
+	public void action() {
+		System.out.printf("I'm playing football for %s team", team);
+
+	}
+
+}
